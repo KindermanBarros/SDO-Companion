1f184bb436e37516db4a27b76ec63c4a429d16a5
Kinderman de Freitas Barros <72263909+KindermanBarros@users.noreply.github.com>
2026-09-10 17:37:13 -0400

    feat: give inventory its own sheet page

--- app/src/test/java/com/kinderman/sdo/presentation/character/CharacterSheetPagerTest.kt
+++ app/src/test/java/com/kinderman/sdo/presentation/character/CharacterSheetPagerTest.kt
@@ -11,4 +11,12 @@ class CharacterSheetPagerTest {
 
         assertEquals(SheetPage.MYSTIC, pages[powersIndex + 1])
     }
+
+    @Test
+    fun `inventory page is immediately to the right of mystic`() {
+        val pages = SheetPage.entries
+        val mysticIndex = pages.indexOf(SheetPage.MYSTIC)
+
+        assertEquals(SheetPage.INVENTORY, pages[mysticIndex + 1])
+    }
 }
