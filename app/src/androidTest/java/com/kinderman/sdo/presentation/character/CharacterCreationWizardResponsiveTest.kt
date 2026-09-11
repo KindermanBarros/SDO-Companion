c8d8ac2f0692a6b5466d4fb87ba5274573066465
Kinderman de Freitas Barros <72263909+KindermanBarros@users.noreply.github.com>
2026-09-11 15:52:52 -0400

    fix: compile responsive instrumented test

--- app/src/androidTest/java/com/kinderman/sdo/presentation/character/CharacterCreationWizardResponsiveTest.kt
+++ app/src/androidTest/java/com/kinderman/sdo/presentation/character/CharacterCreationWizardResponsiveTest.kt
@@ -3,7 +3,6 @@ package com.kinderman.sdo.presentation.character
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.fetchSemanticsNode
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.unit.dp
