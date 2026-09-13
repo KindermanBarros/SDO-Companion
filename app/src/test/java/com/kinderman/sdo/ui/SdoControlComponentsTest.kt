5a690e6ba72fdfaf1b57ed0d8e6955e7c1b5d999
Kinderman de Freitas Barros <72263909+KindermanBarros@users.noreply.github.com>
2026-09-13 15:52:09 -0400

    fix: use configured junit assertions

--- app/src/test/java/com/kinderman/sdo/ui/SdoControlComponentsTest.kt
+++ app/src/test/java/com/kinderman/sdo/ui/SdoControlComponentsTest.kt
@@ -1,7 +1,7 @@
 package com.kinderman.sdo.ui
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class SdoControlComponentsTest {
     @Test
