147b4d5e265d9558468e5a8e38dff663a7a78d2a
Kinderman de Freitas Barros <72263909+KindermanBarros@users.noreply.github.com>
2026-09-15 01:40:09 -0400

    refactor: use Kaltoch mode in settings

--- app/src/main/java/com/kinderman/sdo/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/kinderman/sdo/presentation/settings/SettingsScreen.kt
@@ -108,7 +108,7 @@ fun SettingsScreen(
                         description = SdoVisualMode::description,
                         onSelect = { onPreferencesChange(preferences.copy(visualMode = it)) },
                         optionBadge = { option ->
-                            if (option == SdoVisualMode.CYBERGRUNGE) ExperimentalBadge()
+                            if (option == SdoVisualMode.KALTOCH) ExperimentalBadge()
                         },
                     )
                 }
